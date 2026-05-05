ff9f07c6fc1a3dd769142ff096b275e2d14d95de
JM314155 <muurimakijere4@gmail.com>
2026-05-05 15:26:00 +0300

    Delete model directory

--- model/taajuus.py
+++
@@ -1,9 +0,0 @@
-class FrequencyAnalysis:
-    def __init__(self):
-        self.counts = {'Kivi': 0, 'Sakset': 0, 'Paperi': 0}
-
-    def update(self, move):
-        self.counts[move] += 1
-
-    def predict(self):
-        return max(self.counts, key=self.counts.get)
